21e5ec675664db8d289b9c9ea8a32e5246c89ebd
p1ass <me@p1ass.com>
2024-03-23 23:23:19 +0900

    compressは不要そう

--- workspaces/server/src/routes/index.ts
+++ workspaces/server/src/routes/index.ts
@@ -4,7 +4,6 @@ import { HTTPException } from 'hono/http-exception';
 import { secureHeaders } from 'hono/secure-headers';
 
 import { cacheControlMiddleware } from '../middlewares/cacheControlMiddleware';
-import { compressMiddleware } from '../middlewares/compressMiddleware';
 
 import { adminApp } from './admin';
 import { apiApp } from './api';
@@ -24,7 +23,7 @@ app.use(
     origin: (origin) => origin,
   }),
 );
-app.use(compressMiddleware);
+// app.use(compressMiddleware);
 app.use(cacheControlMiddleware);
 
 app.get('/healthz', (c) => {
