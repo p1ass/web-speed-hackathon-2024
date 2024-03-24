f23eaecf61b1548545474718d753442078f8eaba
p1ass <me@p1ass.com>
2024-03-24 15:57:38 +0900

    magikaを消す

--- workspaces/admin/src/pages/EpisodeCreatePage/index.tsx
+++ workspaces/admin/src/pages/EpisodeCreatePage/index.tsx
@@ -1,21 +1,14 @@
-import { CircularProgress, Flex } from '@chakra-ui/react';
+import {lazy, Suspense} from "react";
 
-import { useBook } from '../../features/books/hooks/useBook';
-import { EpisodeDetailEditor } from '../../features/episodes/components/EpisodeDetailEditor';
-import { episodeCreateRoute } from '../../routes';
 
-export const EpisodeCreatePage: React.FC = () => {
-  const { bookId } = episodeCreateRoute.useParams();
+const EpisodeCreatePage = lazy(() => import('./EpisodeCreatePage'));
 
-  const { data: book } = useBook({ bookId });
-
-  if (book == null) {
-    return (
-      <Flex align="center" height="100%" justify="center" width="100%">
-        <CircularProgress isIndeterminate flexGrow={0} flexShrink={0} size={120} />
-      </Flex>
-    );
-  }
-
-  return <EpisodeDetailEditor book={book} />;
+const EpisodeCreatePageWithSuspense: React.FC = () => {
+  return (
+      <Suspense fallback={null}>
+        <EpisodeCreatePage/>
+      </Suspense>
+  );
 };
+
+export {EpisodeCreatePageWithSuspense as EpisodeCreatePage}
--- workspaces/admin/src/pages/EpisodeDetailPage/index.tsx
+++ workspaces/admin/src/pages/EpisodeDetailPage/index.tsx
@@ -1,23 +1,13 @@
-import { CircularProgress, Flex } from '@chakra-ui/react';
+import {lazy, Suspense} from "react";
 
-import { useBook } from '../../features/books/hooks/useBook';
-import { EpisodeDetailEditor } from '../../features/episodes/components/EpisodeDetailEditor';
-import { useEpisode } from '../../features/episodes/hooks/useEpisode';
-import { episodeDetailRoute } from '../../routes';
+const EpisodeDetailPage = lazy(() => import('./EpisodeDetailPage'));
 
-export const EpisodeDetailPage: React.FC = () => {
-  const { bookId, episodeId } = episodeDetailRoute.useParams();
-
-  const { data: book } = useBook({ bookId });
-  const { data: episode } = useEpisode({ episodeId });
-
-  if (book == null || episode == null) {
-    return (
-      <Flex align="center" height="100%" justify="center" width="100%">
-        <CircularProgress isIndeterminate flexGrow={0} flexShrink={0} size={120} />
-      </Flex>
-    );
-  }
-
-  return <EpisodeDetailEditor book={book} episode={episode} />;
+const EpisodeDetailPageWithSuspense: React.FC = () => {
+  return (
+      <Suspense fallback={null}>
+        <EpisodeDetailPage/>
+      </Suspense>
+  );
 };
+
+export {EpisodeDetailPageWithSuspense as EpisodeDetailPage}
