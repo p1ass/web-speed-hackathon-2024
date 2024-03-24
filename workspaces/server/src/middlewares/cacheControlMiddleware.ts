eaddac73478f95526f6707b8e9b7d04d9d879070
p1ass <me@p1ass.com>
2024-03-24 13:04:54 +0900

    cacheの設定を変更

--- workspaces/server/src/middlewares/cacheControlMiddleware.ts
+++ workspaces/server/src/middlewares/cacheControlMiddleware.ts
@@ -3,8 +3,11 @@ import { createMiddleware } from 'hono/factory';
 export const cacheControlMiddleware = createMiddleware(async (c, next) => {
   await next();
 
-  if (c.req.path.includes("/assets") ||c.req.path.includes("/images")){
-    c.res.headers.append('Cache-Control', 'max-age=1000000');
+  if (c.req.path.includes("/assets") ||
+      c.req.path.includes("/images") ||
+      (c.req.path.includes(".js") && !c.req.path.includes("serviceworker.global.js"))
+  ){
+    c.res.headers.append('Cache-Control', 'public, max-age=1000000');
     return
   }
   c.res.headers.append('Cache-Control', 'private');
