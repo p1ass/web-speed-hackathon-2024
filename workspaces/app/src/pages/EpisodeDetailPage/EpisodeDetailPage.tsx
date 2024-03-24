bbe98959d5709c36478e85094f0c6d2ce2c55f7e
p1ass <me@p1ass.com>
2024-03-24 14:23:00 +0900

    lazy load

---
+++ workspaces/app/src/pages/EpisodeDetailPage/EpisodeDetailPage.tsx
@@ -0,0 +1,40 @@
+import {useParams} from "react-router-dom";
+import type {RouteParams} from "regexparam";
+import invariant from "tiny-invariant";
+
+import {EpisodeListItem} from "../../features/episode/components/EpisodeListItem";
+import {useEpisodeList} from "../../features/episode/hooks/useEpisodeList";
+import {Box} from "../../foundation/components/Box";
+import {Flex} from "../../foundation/components/Flex";
+import {Separator} from "../../foundation/components/Separator";
+import {Space} from "../../foundation/styles/variables";
+
+import {ComicViewer} from "./internal/ComicViewer";
+
+export const EpisodeDetailPage: React.FC = () => {
+  const {bookId, episodeId} = useParams<RouteParams<'/books/:bookId/episodes/:episodeId'>>();
+  invariant(bookId);
+  invariant(episodeId);
+
+  const {data: episodes} = useEpisodeList({query: {bookId}});
+
+  return (
+      <Box>
+        <section aria-label="漫画ビューアー">
+          <ComicViewer episodeId={episodeId}/>
+        </section>
+
+        <Separator/>
+
+        <Box aria-label="エピソード一覧" as="section" px={Space * 2}>
+          <Flex align="center" as="ul" direction="column" justify="center">
+            {episodes.map((episode) => (
+                <EpisodeListItem key={episode.id} bookId={bookId} episode={episode}/>
+            ))}
+          </Flex>
+        </Box>
+      </Box>
+  );
+};
+
+export default EpisodeDetailPage
