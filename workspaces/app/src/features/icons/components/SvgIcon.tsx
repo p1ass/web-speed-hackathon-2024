96b0c33284c842e917857d2b75f759d100c56851
p1ass <me@p1ass.com>
2024-03-24 12:47:06 +0900

    muiを消す

--- workspaces/app/src/features/icons/components/SvgIcon.tsx
+++ workspaces/app/src/features/icons/components/SvgIcon.tsx
@@ -1,14 +1,35 @@
-import * as Icons from '@mui/icons-material';
+import {
+  ArrowBack,
+  Close,
+  Favorite,
+  FavoriteBorder,
+  NavigateNext,
+  Search
+} from '@mui/icons-material';
 
 type Props = {
   color: string;
   height: number;
-  type: keyof typeof Icons;
+  type: 'NavigateNext' | 'Close'|'Search'|'ArrowBack' | 'Favorite'|'FavoriteBorder';
   width: number;
 };
 
 export const SvgIcon: React.FC<Props> = ({ color, height, type, width }) => {
   // eslint-disable-next-line
-  const Icon = Icons[type];
-  return <Icon style={{ color, height, width }} />;
+
+  switch (type) {
+    case "ArrowBack":
+      return <ArrowBack style={{ color, height, width }} />;
+    case "Close":
+      return <Close style={{ color, height, width }} />;
+    case "NavigateNext":
+      return <NavigateNext style={{ color, height, width }} />;
+    case "Search":
+      return <Search style={{ color, height, width }} />;
+    case "Favorite":
+      return <Favorite style={{ color, height, width }} />;
+    case "FavoriteBorder":
+      return <FavoriteBorder style={{ color, height, width }} />;
+  }
+
 };
