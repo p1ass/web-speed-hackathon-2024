0973919a16320f4c52f2de5eef41c18be95a50a2
p1ass <me@p1ass.com>
2024-03-24 15:02:32 +0900

    comic調整

--- workspaces/app/src/pages/EpisodeDetailPage/index.tsx
+++ workspaces/app/src/pages/EpisodeDetailPage/index.tsx
@@ -1,13 +1,24 @@
+import {Skeleton} from "@mui/material";
 import {lazy, Suspense} from 'react';
+
+import {Box} from "../../foundation/components/Box";
 
 const EpisodeDetailPage = lazy(() => import('./EpisodeDetailPage'));
 
 const EpisodeDetailPageWithSuspense: React.FC = () => {
   return (
-      <Suspense fallback={null}>
+      <Suspense fallback={<LoadingComicViewer />}>
         <EpisodeDetailPage/>
       </Suspense>
   );
 };
+
+
+const LoadingComicViewer = ()=>{
+  return <Box>
+    <Skeleton height={650} variant={"rectangular"} width={"100%"}></Skeleton>
+  </Box>
+}
+
 
 export {EpisodeDetailPageWithSuspense as EpisodeDetailPage};
